14ad83661b755567952e3120f82f7435230126c6
mvmcruz <63024612+mvmcruz@users.noreply.github.com>
2021-11-03 13:47:10 -0300

    commited

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask
 from flask_restful import Api
 from db import db
 from flask_cors import CORS, cross_origin
-from dotenv import load_dotenv
 import os
 from controllers.concessionarias_controller import ConcessionariaController
 from controllers.placasolar_controller import PlacaSolarController
 from controllers.orcamentos_controller import OrcamentoController
 from controllers.irradiacaoglobal_controller import IrradiacaoGlobalController
-load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mumlvibqtpxipl:e089c79323642bc6c261807c6cbdf3c6286542d3b92d7e213ff3273a801ad26a@ec2-3-222-24-200.compute-1.amazonaws.com:5432/d3utkbcu4mb99v'
 app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
